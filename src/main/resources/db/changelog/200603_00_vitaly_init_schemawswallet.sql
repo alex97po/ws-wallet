6138755dae528f1ed4b616c40f1509fbd285c777
Otakoe <vitalylarc@gmail.com>
2020-06-03 03:45:47 +0300

    Настроил Liquibase, добавил таблицу credentials. Для удобства выложил пару шаблонов для ченжлога. Файл с SQL на создание бд

---
+++ src/main/resources/db/changelog/200603_00_vitaly_init_schemawswallet.sql
@@ -0,0 +1,3 @@
+-- liquibase formatted sql
+-- changeset vitaly:00
+create schema wswallet;
---
+++ src/main/resources/db/changelog/initialDB_RunBefore_LiquibaseUpdate.sql
@@ -0,0 +1,14 @@
+-- Create in pgAdmin or run here and set this DB in Data source properties
+
+-- Database: wallet_management
+
+-- DROP DATABASE wallet_management;
+
+CREATE DATABASE wallet_management
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Ukrainian_Ukraine.1251'
+    LC_CTYPE = 'Ukrainian_Ukraine.1251'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
