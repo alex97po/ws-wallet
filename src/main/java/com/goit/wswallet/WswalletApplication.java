769073f332dfcdbb2a0742e1c600cd099bc79c83
Otakoe <vitalylarc@gmail.com>
2020-06-03 03:41:47 +0300

    Создал проект

---
+++ src/main/java/com/goit/wswallet/WswalletApplication.java
@@ -0,0 +1,13 @@
+package com.goit.wswallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WswalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WswalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goit/wswallet/WswalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goit.wswallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WswalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
